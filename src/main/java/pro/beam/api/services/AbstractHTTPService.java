6d4f19e4061954e72e2ab1abb7c2e5937f4a25ad
Taylor Blau <me@ttaylorr.com>
2015-02-26 12:22:59 -0500

    Introduce ServiceManager to contain and manage many services

--- src/main/java/pro/beam/api/services/AbstractHTTPService.java
+++ src/main/java/pro/beam/api/services/AbstractHTTPService.java
@@ -2,18 +2,29 @@ package pro.beam.api.services;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import pro.beam.api.BeamAPI;
-import pro.beam.api.http.RequestType;
-
-import java.io.IOException;
+import pro.beam.api.http.BeamHttpClient;
 
 public abstract class AbstractHTTPService extends AbstractBeamService {
+    protected final BeamHttpClient http;
+
     public AbstractHTTPService(BeamAPI beam) {
         super(beam);
+        this.http = this.beam.http();
     }
 
-    public <T> ListenableFuture<T> get(String path, Class<T> type) throws IOException {
-        return this.beam.get(path, type);
+    public <T> ListenableFuture<T> get(String path, Class<T> type) {
+        return this.http.get(path, type);
     }
 
-    public
+    public <T> ListenableFuture<T> post(String path, Class<T> type, Object... args) {
+        return this.http.post(path, type, args);
+    }
+
+    public <T> ListenableFuture<T> put(String path, Class<T> type, Object... args) {
+        return this.http.put(path, type, args);
+    }
+
+    public <T> ListenableFuture<T> delete(String path, Class<T> type) {
+        return this.http.delete(path, type);
+    }
 }
