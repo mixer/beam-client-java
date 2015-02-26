f55562268f7b0734ed4f19225f2b38918bebfea3
Taylor Blau <me@ttaylorr.com>
2015-02-25 21:23:35 -0500

    Further refactorings to make request creation more atomic

--- src/main/java/pro/beam/api/BeamAPI.java
+++ src/main/java/pro/beam/api/BeamAPI.java
@@ -1,13 +1,11 @@
 package pro.beam.api;
 
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.common.util.concurrent.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import pro.beam.api.http.RequestType;
 
 import java.io.IOException;
 import java.net.URI;
@@ -27,12 +25,20 @@ public class BeamAPI {
     }
 
     public <T> ListenableFuture<T> get(String path, Class<T> type) throws IOException {
-        return this.executor.submit(this.callRequest(
-                this.requestFactory.buildGetRequest(
-                    this.buildFromRelativePath(path)), type));
+        return this.executor.submit(this.makeCallable(this.makeRequest(RequestType.GET, path), type));
     }
 
-    private <T> Callable<T> callRequest(final HttpRequest request, final Class<T> type) {
+    private HttpRequest makeRequest(RequestType requestType, String path) throws IOException {
+        return this.makeRequest(requestType, path, null);
+    }
+
+    private HttpRequest makeRequest(RequestType requestType, String path, HttpContent content) throws IOException {
+        return this.requestFactory.buildRequest(requestType.name(),
+                                                this.buildFromRelativePath(path),
+                                                content);
+    }
+
+    private <T> Callable<T> makeCallable(final HttpRequest request, final Class<T> type) {
         return new Callable<T>() {
             @Override public T call() throws Exception {
                 HttpResponse response = request.execute();
---
+++ src/main/java/pro/beam/api/http/RequestType.java
@@ -0,0 +1,5 @@
+package pro.beam.api.http;
+
+public enum RequestType {
+    GET, POST, PUT, DELETE;
+}
