f7c1edeb454bfcf3c0ff74ada0b6e4d5ea457ded
Taylor Blau <me@ttaylorr.com>
2015-07-15 14:53:35 -0400

    Add some service methods to fetch channels by type ID

--- src/main/java/pro/beam/api/services/impl/TypesService.java
+++ src/main/java/pro/beam/api/services/impl/TypesService.java
@@ -3,8 +3,12 @@ package pro.beam.api.services.impl;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.util.concurrent.ListenableFuture;
 import pro.beam.api.BeamAPI;
+import pro.beam.api.response.channels.ShowChannelsResponse;
 import pro.beam.api.response.channels.ShowSlugsRepsonse;
 import pro.beam.api.services.AbstractHTTPService;
+import pro.beam.api.util.Enums;
+
+import java.util.Map;
 
 public class TypesService extends AbstractHTTPService {
     public TypesService(BeamAPI beam) {
@@ -31,5 +35,26 @@ public class TypesService extends AbstractHTTPService {
         }
 
         return this.get("", ShowSlugsRepsonse.class, params.build());
+    }
+
+
+
+    public ListenableFuture<ShowChannelsResponse> channels(int id) {
+        return this.channels(id, 0, 50, null, null, null);
+    }
+
+    public ListenableFuture<ShowChannelsResponse> channels(int id,
+                                                           int page, int limit,
+                                                           ShowChannelsResponse.Attributes orderAttribute, ShowChannelsResponse.Ordering ordering,
+                                                           ShowChannelsResponse.Attributes only) {
+
+        ImmutableMap.Builder<String, Object> arguments = ImmutableMap.builder();
+
+        if (orderAttribute != null) arguments.put("order", Enums.serializedName(orderAttribute)+":"+Enums.serializedName(ordering));
+        if (only != null) arguments.put("only", Enums.serializedName(only));
+        arguments.put("page", Math.max(0, page));
+        arguments.put("limit", Math.min(50, limit));
+
+        return this.get(id+"/channels", ShowChannelsResponse.class, arguments.build());
     }
 }
