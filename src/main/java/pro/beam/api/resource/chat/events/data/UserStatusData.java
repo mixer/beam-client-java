7d156c771dc19a3d225b5dba1fd0532c1bd516ec
Taylor Blau <me@ttaylorr.com>
2016-05-11 22:55:08 -0400

    chat/status: serialize IDs as strings

--- src/main/java/pro/beam/api/resource/chat/events/data/UserStatusData.java
+++ src/main/java/pro/beam/api/resource/chat/events/data/UserStatusData.java
@@ -8,5 +8,5 @@ import java.util.List;
 public class UserStatusData extends AbstractChatEvent.EventData {
     public String username;
     public List<BeamUser.Role> roles;
-    public int id;
+    public String id;
 }
