663dab409695163a5a27c41410c9fe4b55703e9b
rogarMS <rogar@microsoft.com>
2017-04-28 10:46:51 -0700

    add viewers current attribute

--- src/main/java/pro/beam/api/response/channels/ShowChannelsResponse.java
+++ src/main/java/pro/beam/api/response/channels/ShowChannelsResponse.java
@@ -11,6 +11,7 @@ public class ShowChannelsResponse extends ArrayList<BeamChannel> {
         @SerializedName("featured") FEATURED,
         @SerializedName("partnered") PARTNERED,
         @SerializedName("name") NAME,
+        @SerializedName("viewersCurrent") VIEWERS_CURRENT,
         @SerializedName("viewersTotal") VIEWERS_TOTAL,
         @SerializedName("followers") FOLLOWERS,
         @SerializedName("subscribers") SUBSCRIBERS,
